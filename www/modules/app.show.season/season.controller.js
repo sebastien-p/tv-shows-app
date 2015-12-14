1e3a3535d2f95ff69849f8cf1c4486c3a3ae62be
Sebastien PITTION <sp@fing.pro>
2015-12-14 11:06:27 +0100

    Add some raw methods to deal with device calendar

--- www/modules/app.show.season/season.controller.js
+++ www/modules/app.show.season/season.controller.js
@@ -8,9 +8,11 @@
    * The season screen controller.
    * @constructor SeasonController
    * @param {Object} $scope - The associated Angular $scope object.
+   * @param {Object} calendarService - The calendar service.
+   * @param {Object} show - The resolved show JSON object.
    * @param {Object} season - The resolved season JSON object.
    */
-  function SeasonController($scope, season) {
+  function SeasonController($scope, calendarService, show, season) {
     var controller = this;
 
     /**
@@ -18,10 +20,20 @@
      * @property {Object} season
      */
     $scope.season = season;
+
+    controller.hasNotAiredYet = function (episode) { debugger;
+      return moment.utc().isBefore(episode.date); // TODO: check if no date? + move to calendarService
+    };
+
+    controller.addToCalendar = function (episode) { debugger; // TODO: $ionicPopup
+      return calendarService.addEpisode(show, season, episode);
+    };
   }
 
   module.controller('seasonController', [
     '$scope',
+    'calendarService',
+    'show',
     'season',
     SeasonController
   ]);
